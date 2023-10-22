5c37eb9256df2703b2dc4105b2ecf908082f5334
One-Liu <lesterhero2002@gmail.com>
2023-10-22 15:22:04 -0600

    CreateEvent: Remove date and duration fields

--- app/src/main/java/com/teamxticket/xticket/ui/view/CreateEvent.kt
+++ app/src/main/java/com/teamxticket/xticket/ui/view/CreateEvent.kt
@@ -4,12 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.datepicker.CalendarConstraints
-import com.google.android.material.datepicker.DateValidatorPointForward
-import com.google.android.material.datepicker.MaterialDatePicker
-import com.google.android.material.timepicker.MaterialTimePicker
-import com.google.android.material.timepicker.TimeFormat
-import com.teamxticket.xticket.R
 import com.teamxticket.xticket.databinding.ActivityCreateEventBinding
 import com.teamxticket.xticket.ui.view.adapter.BandArtistAdapter
 
@@ -21,54 +15,6 @@ class CreateEvent : AppCompatActivity() {
         binding = ActivityCreateEventBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initRecyclerView()
-
-        binding.btnPickDate.setOnClickListener {
-            val constraintsBuilder = CalendarConstraints.Builder().setValidator(
-                DateValidatorPointForward.now())
-            val datePicker = MaterialDatePicker.Builder.datePicker()
-                .setTitleText(resources.getString(R.string.eventDate))
-                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
-                .setCalendarConstraints(constraintsBuilder.build())
-                .build().apply {
-                    addOnPositiveButtonClickListener {
-                        val selectedDate = this.headerText
-                        binding.eventDate.text = selectedDate
-                    }
-                }
-
-            datePicker.show(supportFragmentManager, "Fecha del evento")
-        }
-
-        binding.btnPickDuration.setOnClickListener {
-            val timePicker = MaterialTimePicker.Builder()
-                .setTimeFormat(TimeFormat.CLOCK_24H)
-                .setHour(0)
-                .setMinute(0)
-                .setTitleText(resources.getString(R.string.eventDuration))
-                .build().apply {
-                    addOnPositiveButtonClickListener {
-
-                        val hours = if (this.hour < 10)
-                            "0${this.hour}"
-                        else
-                            this.hour.toString()
-
-                        val minutes = if (this.minute < 10)
-                            "0${this.minute}"
-                        else
-                            this.minute.toString()
-
-                        val selectedTime = "${hours}:${minutes} horas"
-
-                        if (this.hour < 2)
-                            Toast.makeText(this@CreateEvent, resources.getString(R.string.minDuration), Toast.LENGTH_SHORT).show()
-                        else
-                            binding.eventDuration.text = selectedTime
-                    }
-                }
-
-            timePicker.show(supportFragmentManager, "Duración del evento")
-        }
     }
 
     private fun initRecyclerView() {
