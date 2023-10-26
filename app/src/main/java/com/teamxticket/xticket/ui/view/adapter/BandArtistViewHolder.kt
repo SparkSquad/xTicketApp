b5d3835e4cf2c70ce0191a3e13a89c54e8281f6c
One-Liu <lesterhero2002@gmail.com>
2023-10-26 11:07:59 -0600

    delete unused import

--- app/src/main/java/com/teamxticket/xticket/ui/view/adapter/BandArtistViewHolder.kt
+++ app/src/main/java/com/teamxticket/xticket/ui/view/adapter/BandArtistViewHolder.kt
@@ -1,7 +1,6 @@
 package com.teamxticket.xticket.ui.view.adapter
 
 import android.view.View
-import android.view.View.OnClickListener
 import androidx.recyclerview.widget.RecyclerView
 import com.teamxticket.xticket.databinding.ItemBandArtistBinding
 import com.teamxticket.xticket.data.model.BandArtist
