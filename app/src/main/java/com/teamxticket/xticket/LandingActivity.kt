f552ad30f78b3cede77fb84ef9b83a5e6389d34d
Noxthe <zs20015725@estudiantes.uv.mx>
2023-10-27 01:22:45 -0600

    Landing and Login Activy changed
    added validations for the et on loginactiviy and added the redirect thingy on the landingactivity

--- app/src/main/java/com/teamxticket/xticket/LandingActivity.kt
+++ app/src/main/java/com/teamxticket/xticket/LandingActivity.kt
@@ -1,11 +1,19 @@
 package com.teamxticket.xticket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+
+        val button = findViewById<Button>(R.id.btnSignIn)
+        button.setOnClickListener {
+            val intent = Intent(this, LandingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/teamxticket/xticket/ui/view/LoginActivity.kt
+++ app/src/main/java/com/teamxticket/xticket/ui/view/LoginActivity.kt
@@ -2,11 +2,42 @@ package com.teamxticket.xticket.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.teamxticket.xticket.R
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnSignInFr: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        btnSignInFr = findViewById(R.id.btnSignInFr)
+
+        btnSignInFr.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if (isValidEmail(email) && isValidPassword(password)) {
+                //función de login aquí
+                Toast.makeText(applicationContext, "Has ingresado de manera exitosa!", Toast.LENGTH_SHORT)
+            } else {
+                Toast.makeText(applicationContext, "El email o contraseña ingresados no son correctos, por favor intente de nuevo.", Toast.LENGTH_LONG)
+            }
+        }
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    private fun isValidPassword(password: String): Boolean {
+        return password.length >= 8
     }
 }
