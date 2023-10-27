809a1e637b38f8045026b827f7b16e450983a6ca
One-Liu <lesterhero2002@gmail.com>
2023-10-27 02:43:49 -0600

    CreateEvent: Conexion con api

---
+++ app/src/main/java/com/teamxticket/xticket/data/model/EventResponse.kt
@@ -0,0 +1,6 @@
+package com.teamxticket.xticket.data.model
+
+data class EventResponse (
+    val message: String,
+    val events: List<Event>
+)
--- app/src/main/java/com/teamxticket/xticket/ui/viewModel/EventViewModel.kt
+++ app/src/main/java/com/teamxticket/xticket/ui/viewModel/EventViewModel.kt
@@ -1,4 +1,51 @@
 package com.teamxticket.xticket.ui.viewModel
 
-class EventViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamxticket.xticket.data.EventRepository
+import com.teamxticket.xticket.data.model.Event
+import kotlinx.coroutines.launch
+
+class EventViewModel : ViewModel() {
+    private val repository = EventRepository()
+    var eventModel = MutableLiveData<List<Event>?>()
+    var genresModel = MutableLiveData<List<String>?>()
+    var showLoader = MutableLiveData<Boolean>()
+    var showLoaderRegister = MutableLiveData<Boolean>()
+    var showLoaderGenres = MutableLiveData<Boolean>()
+    var successfulRegister = MutableLiveData<Int>()
+
+    fun loadEvents(userId: Int) {
+        viewModelScope.launch {
+            showLoader.postValue(true)
+            val result = repository.getAllEvents(userId)
+            if (result.isNotEmpty()) {
+                eventModel.postValue(result)
+            } else {
+                eventModel.postValue(null)
+            }
+            showLoader.postValue(false)
+        }
+    }
+
+    fun registerEvent(event: Event) {
+        viewModelScope.launch {
+            showLoaderRegister.postValue(true)
+            val result = repository.postEvent(event)
+            successfulRegister.postValue(result)
+            showLoaderRegister.postValue(false)
+        }
+    }
+
+    fun loadGenres() {
+        viewModelScope.launch {
+            showLoaderGenres.postValue(true)
+            val result = repository.getGenres()
+            if (result.isNotEmpty()) {
+                genresModel.postValue(result)
+                showLoaderGenres.postValue(false)
+            }
+        }
+    }
 }
